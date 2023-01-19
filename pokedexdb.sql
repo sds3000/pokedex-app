60d1ab4862b7096836caa5b0492ed9c378894118
sds3000 <64442811+sds3000@users.noreply.github.com>
2023-01-19 18:22:39 -0500

    initial commit

---
+++ pokedexdb.sql
@@ -0,0 +1,83 @@
+DROP TABLE IF EXISTS pokemon;
+
+CREATE TABLE pokemon
+(
+  pokemon_id     SERIAL   PRIMARY KEY NOT NULL,
+  name           TEXT       NOT NULL,
+  summary        TEXT,
+  next_evolution TEXT      ,
+  type_id        SERIAL     NOT NULL,
+);
+
+DROP TABLE IF EXISTS type;
+
+CREATE TABLE type
+(
+  type_id SERIAL NOT NULL,
+  name    TEXT   NOT NULL,
+  PRIMARY KEY (type_id)
+);
+
+DROP TABLE IF EXISTS image;
+
+CREATE TABLE image
+(
+  image_id   SERIAL  NOT NULL,
+  image      BOOLEAN,
+  pokemon_id INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS moves;
+
+CREATE TABLE moves
+(
+  moves_id   SERIAL  NOT NULL,
+  name       TEXT    NOT NULL,
+  atk        INTEGER,
+  pp         INTEGER,
+  pokemon_id INTEGER NOT NULL,
+  type_id    SERIAL  NOT NULL
+);
+
+DROP TABLE IF EXISTS stats;
+
+CREATE TABLE stats
+(
+  stats_id   SERIAL  NOT NULL,
+  weight     INTEGER,
+  type_id    INTEGER,
+  egg_steps  INTEGER,
+  type_id    SERIAL  NOT NULL,
+  pokemon_id INTEGER NOT NULL
+);
+
+
+ALTER TABLE image
+  ADD CONSTRAINT FK_pokemon_TO_image
+    FOREIGN KEY (pokemon_id)
+    REFERENCES pokemon (pokemon_id);
+
+ALTER TABLE moves
+  ADD CONSTRAINT FK_pokemon_TO_moves
+    FOREIGN KEY (pokemon_id)
+    REFERENCES pokemon (pokemon_id);
+
+ALTER TABLE stats
+  ADD CONSTRAINT FK_type_TO_stats
+    FOREIGN KEY (type_id)
+    REFERENCES type (type_id);
+
+ALTER TABLE moves
+  ADD CONSTRAINT FK_type_TO_moves
+    FOREIGN KEY (type_id)
+    REFERENCES type (type_id);
+
+ALTER TABLE stats
+  ADD CONSTRAINT FK_pokemon_TO_stats
+    FOREIGN KEY (pokemon_id)
+    REFERENCES pokemon (pokemon_id);
+
+ALTER TABLE pokemon
+  ADD CONSTRAINT FK_type_TO_pokemon
+    FOREIGN KEY (type_id)
+    REFERENCES type (type_id);
