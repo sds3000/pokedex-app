9080dc56f364e2d0e41e5a919dcab75302ddb749
sds3000 <64442811+sds3000@users.noreply.github.com>
2023-01-22 15:43:25 -0500

    Database
    started work on teams db

--- pokedexdb.sql
+++ pokedexdb.sql
@@ -1,82 +1,34 @@
-DROP TABLE IF EXISTS pokemon;
+DROP TABLE IF EXISTS team;
 
 CREATE TABLE team
 (
   pokemon_id     SERIAL   PRIMARY KEY NOT NULL,
   name           TEXT       NOT NULL,
-  next_evolution TEXT      ,
-  type_id        SERIAL     NOT NULL,
+  next_evolution TEXT,
+  type        SERIAL     NOT NULL,
+
 );
 
-DROP TABLE IF EXISTS type;
+DROP TABLE IF EXISTS caught;
 
-CREATE TABLE type
+create TABLE caught 
 (
-  type_id SERIAL NOT NULL,
-  name    TEXT   NOT NULL,
-  PRIMARY KEY (type_id)
-);
-
-DROP TABLE IF EXISTS image;
-
-CREATE TABLE image
-(
-  image_id   SERIAL  NOT NULL,
-  image      BOOLEAN,
-  pokemon_id INTEGER NOT NULL
-);
-
-DROP TABLE IF EXISTS moves;
-
-CREATE TABLE moves
-(
-  moves_id   SERIAL  NOT NULL,
-  name       TEXT    NOT NULL,
-  atk        INTEGER,
-  pp         INTEGER,
-  pokemon_id INTEGER NOT NULL,
-  type_id    SERIAL  NOT NULL
+  caught_id SERIAL NOT NULL,
+  
 );
 
 DROP TABLE IF EXISTS stats;
 
 CREATE TABLE stats
 (
-  stats_id   SERIAL  NOT NULL,
-  weight     INTEGER,
-  type_id    INTEGER,
-  egg_steps  INTEGER,
-  type_id    SERIAL  NOT NULL,
-  pokemon_id INTEGER NOT NULL
+  stat_id        SERIAL  NOT NULL,
+  pokemon_id INTEGER NOT NULL,
+  curr_level INTEGER NOT NULL,
+  next_evolution_level INTEGER,
+  attack INTEGER NOT NULL,
+  speed INTEGER NOT NULL,
+  defense INTEGER NOT NULL,
+  special_defense INTEGER NOT NULL,
+  special_attack INTEGER NOT NULL,
+  total INTEGER NOT NULL
 );
-
-
-ALTER TABLE image
-  ADD CONSTRAINT FK_pokemon_TO_image
-    FOREIGN KEY (pokemon_id)
-    REFERENCES pokemon (pokemon_id);
-
-ALTER TABLE moves
-  ADD CONSTRAINT FK_pokemon_TO_moves
-    FOREIGN KEY (pokemon_id)
-    REFERENCES pokemon (pokemon_id);
-
-ALTER TABLE stats
-  ADD CONSTRAINT FK_type_TO_stats
-    FOREIGN KEY (type_id)
-    REFERENCES type (type_id);
-
-ALTER TABLE moves
-  ADD CONSTRAINT FK_type_TO_moves
-    FOREIGN KEY (type_id)
-    REFERENCES type (type_id);
-
-ALTER TABLE stats
-  ADD CONSTRAINT FK_pokemon_TO_stats
-    FOREIGN KEY (pokemon_id)
-    REFERENCES pokemon (pokemon_id);
-
-ALTER TABLE pokemon
-  ADD CONSTRAINT FK_type_TO_pokemon
-    FOREIGN KEY (type_id)
-    REFERENCES type (type_id);
--- server/server.js
+++ server/server.js
@@ -3,14 +3,31 @@ import postgres from 'postgres'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
+dotenv.config() // applies env variables
 
 let app = express();
-let sql = postgres(process.env)// .URL
+let sql = postgres(process.env);// .URL if deployed
+let port = process.env.PORT;
+
+// MIDDLEWARE ================================
 
 app.use(express.json());
 app.use(cors()); //cross browser
 app.use(express.static('./src'));
 
+// ROUTES =====================================
 /*
 * fetch data from api
 */
+
+app.get('/', (req, res) => { // used to get saved teams from db
+    sql`SELECT * FROM team`.then(result => res.json(result))
+});
+
+app.delete('/delete-pokemon', (req,res) => {
+
+});
+
+app.listen(port, () => {
+    console.log("10 years old for 20+ years... HOW???!!! -_-")
+});
